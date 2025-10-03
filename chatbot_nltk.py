d9c09882c5e11ffd85b3c053debcec5b8786802b
Joshua <Jay.ibidapo@gmail.com>
2025-10-03 02:54:22 -0500

    Update chatbot_nltk.py
    Add initial chatbot patterns and responses for handling greetings and user queries
    
    Define rule-based conversation pairs for chatbot interaction
    
    Add basic user input patterns and responses to chatbot

--- chatbot_nltk.py
+++ chatbot_nltk.py
@@ -1,2 +1,21 @@
 from nltk.chat.util import Chat, reflections
 
+#Pairs is a list of patterns and responses.
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is thecleverprogrammer, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
